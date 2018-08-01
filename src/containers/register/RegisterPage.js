1153580d7fc976d406cf3f37d65cbe5e92608a91
Moshe Vilner <moshe.vilner@webintpro.com>
2018-08-01 14:58:46 +0300

    styling register differently

--- src/containers/register/RegisterPage.js
+++ src/containers/register/RegisterPage.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import '../login/login.css';
 
 const RegisterPage = () => {
     return (
-        <div>register page</div>
+        <div className="container">
+            <form  name="registerForm" className="form-signin well">
+                <h2 className="form-signin-heading">Register</h2>
+
+                <div className="control-group">
+                    <div className="controls">
+                        <input type="text" placeholder="Username" name="username" className="input-block-level" />               
+                    </div>
+                </div>
+
+                <div className="control-group">
+                    <div className="controls">
+                        <input type="password" placeholder="Password" name="password"  className="input-block-level" />                               
+                    </div>
+                </div>
+
+                <label for="rememberme" className="checkbox">
+                    <input type="radio"  name="level"/>Administrator
+                </label>
+                <label for="rememberme" className="checkbox">
+                    <input type="radio" name="level"/>Normal User
+                </label>
+
+                <div className="control-group">
+                    <div className="controls">
+                        <button type="submit" className="btn btn-large btn-primary" disabled="disabled">Submit</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
     )
 }
 export default RegisterPage
