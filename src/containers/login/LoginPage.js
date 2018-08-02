068486cba8b30689ce52b8bbec8631f84e7e9b19
Moshe Vilner <moshe.vilner@webintpro.com>
2018-08-02 09:58:58 +0300

    login apge should send rquest

--- src/containers/login/LoginPage.js
+++ src/containers/login/LoginPage.js
@@ -1,39 +1,91 @@
-import React from 'react'
+import React, {Component} from 'react'
 import  './login.css'
 
-const LoginPage = () => {
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.reload = false;
+    this.state = {
+      username: '',
+      password: '',
+      rememberme: true
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.send = this.send.bind(this);
+  }
 
-  return (
-    <div className="container">
-          <form  name="loginForm" className="form-signin well">
-            <h2 className="form-signin-heading">Please log in</h2>
+  handleInputChange(event) {
 
-            <div className="control-group">
-                <div className="controls">
-                  <input type="text" placeholder="Username" name="username" className="input-block-level" />               
-                </div>
-            </div>
+    const {target:{value, name}} = event;
+    console.log('nm:', name)
+    this.setState({
+      [name]: value
+    });  
 
-            <div className="control-group">
-                <div className="controls">
-                  <input type="password" placeholder="Password" name="password"  className="input-block-level" />                               
-                </div>
-            </div>
+  }
 
-            <label for="rememberme" className="checkbox">
-                <input type="checkbox" data-ng-model="rememberme" name="rememberme"/>Remember me
-            </label>
+  send(e) {
+    e.preventDefault();
+    // validation
+    if ( this.state.username.trim() === '' || this.state.password.trim() === '' ) { return; }
+    const data = this.state;
 
-            <div className="control-group">
-                <div className="controls">
-                  <button type="submit" className="btn btn-large btn-primary" disabled="disabled">Log in</button>
-                </div>
-            </div>
+    fetch('http://localhost:8000/login', {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify( data ) // body data type must match "Content-Type" header
+    })
+    .then(response => {
+      response.json();
+      this.setState({
+        username: '',
+        password: ''
+      });
+      
+    }) // parses response to JSON
+    .catch(error => console.error(`Fetch Error =\n`, error));
+  }
 
-          </form>
-    </div>
+  render() {
+    return (
+      <div className="container">
+            <form  name="loginForm" className="form-signin well">
+              <h2 className="form-signin-heading">Please log in</h2>
 
-  )
+              <div className="control-group">
+                  <div className="controls">
+                    <input type="text" placeholder="Username" name="username" value={this.state.username}  onChange={this.handleInputChange} className="input-block-level" />               
+                  </div>
+              </div>
+
+              <div className="control-group">
+                  <div className="controls">
+                    <input type="password" placeholder="Password" name="password" value={this.state.password}    onChange={this.handleInputChange} className="input-block-level" />                               
+                  </div>
+              </div>
+
+              <label for="rememberme" className="checkbox">
+                  <input type="checkbox" data-ng-model="rememberme" name="rememberme"/>Remember me
+              </label>
+
+              <div className="control-group">
+                  <div className="controls">
+                    <button className="btn btn-large btn-primary" onClick={this.send}>Log in</button>
+                  </div>
+              </div>
+
+            </form>
+      </div>
+
+    )
+  }
 }
 
 
